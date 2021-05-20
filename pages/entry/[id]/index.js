2ea6f160bce20ee542e864968c6179bb3f5e8d56
dominiclet <dominic.let@hotmail.com>
2021-05-20 22:27:16 +0800

    styling for case summary

--- pages/entry/[id]/index.js
+++ pages/entry/[id]/index.js
@@ -1,46 +1,16 @@
-import {useRouter} from 'next/router'
-import Link from 'next/link'
-import caseStyle from '../../../styles/Case.module.css'
-import TextBox from '../../../components/TextBox'
+import {useRouter} from 'next/router';
+import Link from 'next/link';
+import OuterCase from '../../../components/casesummary/OuterCase';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const entry = ({entry}) => {
 
     return (
-        <>
-            <div className={caseStyle.casesummary}>
-                <header className={caseStyle.header}>
-                    <h1>Case Name</h1>
-                </header>
-                <TextBox/>
-                <TextBox/>
-            </div>
+        <div>
+            <OuterCase />
             <Link href='/'>Go Back</Link>
-        </>
+        </div>
     )
-}
-
-export const getStaticProps = async (context) => {
-    const res = await fetch (`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
-
-    const entry = await res.json()
-    return {
-        props: {
-            entry
-        }
-    }
-}
-
-export const getStaticPaths = async () => {
-    const res = await fetch (`https://jsonplaceholder.typicode.com/posts`)
-
-    const entries = await res.json()
-    const ids = entries.map(entry => entry.id)
-    const paths = ids.map(id => ({params: {id: id.toString()}}))
-
-    return {
-        paths, 
-        fallback: false
-    }
 }
 
 export default entry

54cc831ace7fa98ece252ce86f17adf7b5a9431b
dominiclet <58429250+dominiclet@users.noreply.github.com>
2021-06-25 10:49:26 +0800

    Revert "Dom"

--- pages/case/[id]/edit/index.js
+++ pages/case/[id]/edit/index.js
@@ -1,59 +1,33 @@
 import { useRouter } from 'next/router';
-import TitleEditor from '../../../../components/casesummary/edit/TitleEditor';
-import caseEditStyle from '../../../../styles/CaseEdit.module.css';
-import axios from 'axios';
-import { apiRoot } from '../../../../config';
-import { useEffect, useState } from 'react';
-import EditorBuilder from '../../../../components/casesummary/edit/EditorBuilder';
+import CaseEditor from '../../../../components/casesummary/edit/CaseEditor';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import caseEditStyle from '../../../../styles/CaseEdit.module.css'
 
 const caseEditPage = () => {
     const router = useRouter();
-
-    // State to store case data
-    const [caseData, setCaseData] = useState();
-    // State to check if data has loaded
-    const [dataLoaded, setdataLoaded] = useState(false);
-
-    // Get case data
-    useEffect( () => {
-        if (router.isReady) {
-            const {id} = router.query;
-            axios.get(apiRoot + `/cases/${id}`)
-            .then(res => {
-                setCaseData(res.data);
-                setdataLoaded(true);
-            })
-            .catch(error => console.log(error));
-        } else return;
-    }, [router.isReady]);
+    const {id} = router.query;
 
     // Reduce padding for Jumbotron
     const jumboStyle = {
         "padding": "1rem 2rem",
         "width": "60vw",
         "margin": "auto",
+        "minWidth": "1000px"
     };
-
-    if (!dataLoaded) {
-        // If data is not loaded yet, display "loading"
-        return (<span>Loading...</span>);
-    } else {
-        const date = new Date(caseData.lastEdit);
-
-        return(
-        <div className={caseEditStyle.editor}>
-            <p className={caseEditStyle.editTimeStamp}>
-                Last edited by [someone] at {date.toLocaleTimeString("en-SG")} on {date.toLocaleDateString("en-SG")}
-            </p>
-            <TitleEditor 
-            caseId={caseData._id} 
-            caseName={caseData.name} 
-            citation={caseData.citation} 
-            />
-            <EditorBuilder caseId={caseData._id} facts={caseData.facts}
-            holding={caseData.holding} />
+    
+    return(
+        <div>
+            <Jumbotron style={jumboStyle}>
+                <h1>Case name</h1>
+                <p>Case citation</p>
+            </Jumbotron>
+            <div className="center">
+                <h3 className={caseEditStyle.header}>Facts</h3>
+                <CaseEditor subTopic="Sub-fact"/>
+                <h3 className={caseEditStyle.header}>Holding</h3>
+                <CaseEditor subTopic="Sub-holding"/>
+            </div>
         </div>
-        );
-    }
+    );
 }
  export default caseEditPage
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Head from 'next/head'
 import EntryList from '../components/homepage/EntryList'
 import Header from '../components/Header'
@@ -6,61 +5,25 @@ import Banner from '../components/homepage/Banner'
 
 // Props: entries (all case entries)
 export default function Home(props) {
-=======
-import Head from 'next/head';
-import EntryList from '../components/EntryList';
-import ActivityCard from '../components/home/ActivityCard';
-import RecentEditCard from '../components/home/RecentEditCard';
-import homeStyle from '../styles/Home.module.css';
-import { apiRoot } from '../config';
-import axios from 'axios';
-
-export default function Home(props) {
-  
-  // Build recent activity
-  let recentActivityBuilder = [];
-  props.data.map((activity) => {
-    recentActivityBuilder.push(<ActivityCard 
-      caseId={activity.id}
-      caseName={activity.case_name}
-      action={activity.action}
-      subtopic={activity.subtopic}
-      time={activity.time}
-    />);
-  })
-
->>>>>>> dom
   return (
-    <div className={homeStyle.container1}>
+    <div>
       <Head>
-        <title>David</title>
+        <title>Lawmology</title>
       </Head>
-<<<<<<< HEAD
       <Banner/>
       <EntryList entries = {props.entries} />
-=======
-      <h4>Your recent edits</h4>
-      <RecentEditCard />
-      <br/>
-      <h4>Recent activity</h4>
-      {recentActivityBuilder}
->>>>>>> dom
     </div>
   )
 }
 
-<<<<<<< HEAD
 // fetching data
 const defaultEndPoint = `http://localhost:5000/cases`;
 export const getServerSideProps = async() => {
   const res = await fetch (defaultEndPoint)
   const entries = await res.json()
-=======
-export async function getStaticProps() {
-  const res = await axios.get(apiRoot + '/recentActivity');
-  const data = res.data;
->>>>>>> dom
   return {
-    props: { data }
+    props: {
+      entries
+    }
   }
 }

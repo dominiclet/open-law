563d8551bdc56962623d272e1e659b1797121f6a
dominiclet <dominic.let@hotmail.com>
2021-07-01 15:41:58 +0800

    added more form validation for register page

--- pages/register/index.js
+++ pages/register/index.js
@@ -8,9 +8,10 @@ const registerPage = () => {
 		"fontSize": "0.8rem"
 	};
 
-	// Styling for text checking matching passwords
-	const matchingPasswordStyle = {
-		"transitionDuration": "0.4s"
+	// Styling for form validation warnings 
+	const warningStyle = {
+		"fontSize": "0.8rem",
+		"color": "red"
 	}
 
 	// Check matching passwords
@@ -24,6 +25,45 @@ const registerPage = () => {
 		} 
 	}
 
+	// Handle submit button
+	const handleSubmit = () => {
+		document.getElementById("fullNameNote").innerHTML = null;
+		document.getElementById("usernameNote").innerHTML = null;
+		document.getElementById("passwordNote").innerHTML = null;
+		document.getElementById("confirmpwNote").innerHTML = null;
+		let noError = true;
+		if (document.getElementById("name").value == 0) {
+			// Check that full name is not empty
+			document.getElementById("fullNameNote").innerHTML = "Full Name is required.";
+			noError = false;
+		}
+		if (document.getElementById("username").value == 0) {
+			// Check that username is not empty
+			document.getElementById("usernameNote").innerHTML = "Username is required.";
+			noError = false;
+		}
+		if (document.getElementById("username").value.includes(" ")) {
+			// Do not allow spaces in username
+			document.getElementById("usernameNote").innerHTML = "No spaces in username.";
+			noError = false;
+		}
+		if (document.getElementById("username").value.length > 30) {
+			// Limit length of username
+			document.getElementById("usernameNote").innerHTML = "Username must be less than 30 characters long.";
+			noError = false;
+		}
+		if (document.getElementById("password").value.length < 8) {
+			// Check that password is at least 8 characters long
+			document.getElementById("passwordNote").innerHTML = "Password must be at least 8 characters long.";
+			noError = false;
+		}
+		if (document.getElementById("password").value !== document.getElementById("confirmpw").value) {
+			// Check that confirm password is the same as password
+			document.getElementById("confirmpwNote").innerHTML = "Password does not match.";
+			noError = false;
+		}
+	}
+
 	return (
 		<div className={styles.formContainer}>
 			<h4>Register</h4>
@@ -31,6 +71,7 @@ const registerPage = () => {
 				<Form.Group controlId="name">
 					<Form.Label>Full name</Form.Label>
 					<Form.Control type="text" placeholder="Enter full name" />
+					<Form.Text id="fullNameNote" style={warningStyle}></Form.Text>
 				</Form.Group>
 				<Form.Group controlId="academicYear">
 					<Form.Label>Academic year</Form.Label>
@@ -48,6 +89,7 @@ const registerPage = () => {
 				<Form.Group controlId="username">
 					<Form.Label>Username</Form.Label>
 					<Form.Control type="text" placeholder="Enter username" />
+					<Form.Text id="usernameNote" style={warningStyle}></Form.Text>
 					<Form.Text className="text-muted" style={mutedTextStyle}>
 						This will be your display name, as well as the username you use to login.
 					</Form.Text>
@@ -55,12 +97,14 @@ const registerPage = () => {
 				<Form.Group controlId="password">
 					<Form.Label>Password</Form.Label>
 					<Form.Control id="password" type="password" placeholder="Enter password" onChange={handleEnterPassword} />
+					<Form.Text id="passwordNote" style={warningStyle}></Form.Text>
 				</Form.Group>
 				<Form.Group className={styles.confirmPassword} controlId="confirmPassword">
 					<Form.Label>Confirm password</Form.Label>
 					<Form.Control type="password" placeholder="Enter password again" id="confirmpw" onChange={handleEnterPassword} />
+					<Form.Text id="confirmpwNote" style={warningStyle}></Form.Text>
 				</Form.Group>
-				<Button type="submit">Submit</Button>
+				<Button onClick={handleSubmit}>Submit</Button>
 			</Form>
 		</div>
 	);

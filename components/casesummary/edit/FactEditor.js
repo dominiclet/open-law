d7e670614bda758d22709be86ecbd2b7ec2741a9
dominiclet <dominic.let@hotmail.com>
2021-07-30 22:32:23 +0800

    added diff comparison for facts

--- components/casesummary/edit/FactEditor.js
+++ components/casesummary/edit/FactEditor.js
@@ -1,11 +1,12 @@
 import dynamic from 'next/dynamic';
 import Button from 'react-bootstrap/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import caseEditStyle from '../../../styles/CaseEdit.module.css';
 import { apiRoot } from '../../../config';
 import axios from 'axios';
 import { PlusLg, Trash, Upload } from 'react-bootstrap-icons';
 import { useRouter } from 'next/router';
+import { Modal } from 'react-bootstrap';
 
 // ReactQuill is only imported at client side 
 // due to issues with next's server-side rendering
@@ -22,10 +23,13 @@ const FactEditor = (props) => {
     // props.setData: Call back function to change state of facts in parent
     // props.caseId: Unique ID of case
 
+    const Diff = require('diff');
     const router = useRouter();
 
     // State to track the changes to fact data internally
-    const [factData, setFactData] = useState(props.data);
+    const [factData, setFactData] = useState(JSON.parse(JSON.stringify(props.data)));
+    // State to control showing diff modal
+    const [showDiffModal, setShowDiffModal] = useState(false);
     
     // Width needs to be fixed otherwise text causes DOM elements to resize
     const styling = {
@@ -100,24 +104,78 @@ const FactEditor = (props) => {
         document.getElementById("factsUpload").style.color = "red";
     }
 
-    // Handle submit functionality
+    // Handle upload functionality: shows the confirm changes modal
     const handleSubmit = (event) => {
+        setShowDiffModal(true);
+    }
+
+    // Handles the submit functionality, after confirm changes is shown
+    const handleActualSubmit = (event) => {
         const data = {
             factData: factData,
             time: new Date().toJSON()
         }
-
         axios.post(apiRoot + `/editSubTopic/${props.caseId}/facts`,
             data, {
                 headers: {'Authorization': 'Bearer ' + localStorage.getItem("jwt-token")}
             }).then(res => {
                 if (res.status == 200) {
                    document.getElementById("factsUpload").style.color = "black";
+                   // Update the parent data
+                   props.setData(JSON.parse(JSON.stringify(factData)));
+                   // Close modal
+                   setShowDiffModal(false);
                 }
             }).catch(e => {
                 throw e;
             });
     }
+
+    // Ensures that this only runs when modal is up
+    useEffect(() => {
+        // Run diff algorithm to check changes
+        if (showDiffModal) {
+            let span = null;
+            const display = document.getElementById("modalContent");
+
+            for (let i = 0; i < Math.max(props.data.length, factData.length); i++) {
+                // Compare title
+                let originalTitle = i < props.data.length ? props.data[i].title : "";
+                let changedTitle = i < factData.length ? factData[i].title : "";
+                
+                const diffTitle = Diff.diffWords(originalTitle, changedTitle);
+                const divTitle = document.createElement("div");
+                
+                diffTitle.forEach(part => {
+                    const color = part.added ? 'green' :
+                    part.removed ? 'red' : 'grey';
+                    span = document.createElement('span');
+                    span.style.color = color;
+                    span.style.fontWeight = "500";
+                    span.appendChild(document.createTextNode(part.value));
+                    divTitle.appendChild(span);
+                });
+                display.appendChild(divTitle);
+
+                // Compare content
+                let originalContent = i < props.data.length ? props.data[i].content : "";
+                let changedContent = i < factData.length ? factData[i].content : "";
+
+                const diffContent = Diff.diffWords(originalContent, changedContent);
+                const divContent = document.createElement("div");
+
+                diffContent.forEach(part => {
+                    const color = part.added ? 'green' :
+                    part.removed ? 'red' : 'grey';
+                    span = document.createElement('span');
+                    span.style.color = color;
+                    span.appendChild(document.createTextNode(part.value));
+                    divContent.appendChild(span);
+                });
+                display.appendChild(divContent);
+            }
+        }
+    }, [showDiffModal])
 
     return (
         <div className={caseEditStyle.topicOuterContainer}>
@@ -134,6 +192,22 @@ const FactEditor = (props) => {
                 className={caseEditStyle.editorSubmitButton}
                 onClick={handleSubmit}
             />
+            <Modal
+                show={showDiffModal}
+                onHide={() => setShowDiffModal(false)}
+                dialogClassName={caseEditStyle.modalContainer}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm fact changes</Modal.Title>
+                </Modal.Header>
+                <Modal.Body id="modalContent">
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={handleActualSubmit}>
+                        Submit
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }

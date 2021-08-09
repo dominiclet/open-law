ed1a75d07728d8359a2b2322d867a80094c04cce
dominiclet <dominic.let@hotmail.com>
2021-08-09 11:08:20 +0800

    minor changes

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
-export const apiRoot = "https://lawmologyapi.herokuapp.com";
-// export const apiRoot = "http://localhost:5000"
+// export const apiRoot = "https://lawmologyapi.herokuapp.com";
+export const apiRoot = "http://localhost:5000"
 export const allTags = [
 	"Tort", 
 	"Contract", 
 	"Equity", 
 	"Evidence",
-	"Intellectual Property"
+	"Intellectual Property",
+	"International Arbitration",
+	"Criminal Law",
+	"Arbitration",
+	"Tax"
 ]
